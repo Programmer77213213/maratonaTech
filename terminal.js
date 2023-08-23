1c0a2558e3766418ccc67231df09a0265369e49d
Programmer77213213 <112766352+Programmer77213213@users.noreply.github.com>
2023-08-23 11:48:38 -0300

    commit one
    site para apresentação da maratona tech.

---
+++ terminal.js
@@ -0,0 +1,5 @@
+var elementbox1 = window.document.querySelector("div#box8");
+elementbox1.addEventListener(`click`, openwindow1);
+function openwindow1(){
+    elementbox1.style.display = `none`;    
+}
